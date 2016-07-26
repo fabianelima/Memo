b4a07f7b381010e59d2de027f1aac62653f54a2c
fabianelima <fabianelim@gmail.com>
2016-07-26 10:43:33 -0300

    Cartas e + progresso :thought_balloon:
    Cartas e algumas outras mecânicas foram feitas, mas o troço ainda não se
    comporta do jeito que eu quero.

--- assets/script.coffee
+++ assets/script.coffee
@@ -1,28 +1,41 @@
 $ ->
 	cards = [
 				[
-					'carta1', 'carta2', 'carta3', 'carta4', 'carta5', 'carta6', 'carta7', 'carta8'
+					'<img class="verso" src="assets/img/verso.png"><img src="assets/img/c1.png">',
+					'<img class="verso" src="assets/img/verso.png"><img src="assets/img/c2.png">',
+					'<img class="verso" src="assets/img/verso.png"><img src="assets/img/c3.png">',
+					'<img class="verso" src="assets/img/verso.png"><img src="assets/img/c4.png">',
+					'<img class="verso" src="assets/img/verso.png"><img src="assets/img/c5.png">',
+					'<img class="verso" src="assets/img/verso.png"><img src="assets/img/c6.png">',
+					'<img class="verso" src="assets/img/verso.png"><img src="assets/img/c7.png">',
+					'<img class="verso" src="assets/img/verso.png"><img src="assets/img/c8.png">'
 				],
 				[
-					'cartaA', 'cartaB', 'cartaC', 'cartaD', 'cartaE', 'cartaF', 'cartaG', 'cartaH'
+					'<img class="verso" src="assets/img/verso.png"><img src="assets/img/c1.png">',
+					'<img class="verso" src="assets/img/verso.png"><img src="assets/img/c2.png">',
+					'<img class="verso" src="assets/img/verso.png"><img src="assets/img/c3.png">',
+					'<img class="verso" src="assets/img/verso.png"><img src="assets/img/c4.png">',
+					'<img class="verso" src="assets/img/verso.png"><img src="assets/img/c5.png">',
+					'<img class="verso" src="assets/img/verso.png"><img src="assets/img/c6.png">',
+					'<img class="verso" src="assets/img/verso.png"><img src="assets/img/c7.png">',
+					'<img class="verso" src="assets/img/verso.png"><img src="assets/img/c8.png">'
 				]
 			]
 
+	score = 0
+
+	### Inicia o jogo ###
 	$('.start').on 'click', ->
 		$(this).parent().fadeOut()
 		$('.game').fadeIn()
-		getgame()
 
-	getgame = ->
 		allCards = cards[0].concat(cards[1])
 		shuffleCards = fisherYates(allCards)
 
 		for i in [0...shuffleCards.length]
 			$('.cards .card' + i).html(shuffleCards[i])
 
-		console.log(shuffleCards)
-
-	# Algoritmo de 
+	### Algoritmo de ordenação Fisher Yates ###
 	fisherYates = (arr) ->
 		i = arr.length
 		if i == 0 then return false
@@ -32,3 +45,21 @@ $ ->
 			[arr[i], arr[j]] = [arr[j], arr[i]]
 		return arr
 
+	for i in [0...16]
+		l = 0
+		$('.card' + i).on 'click', ->
+			l++
+			k = $(this).attr('class').split('card')[1]
+			$('.card' + k + ' .verso').hide()
+			$('.card' + k).addClass('escolha' + l)
+
+			if l > 0 and l < 3			# daqui pra baixo nada deu certo
+				console.log(l)
+				$('.cards').css('pointer-events','none')
+				if $('.escolha1') == $('.escolha2')
+					score++
+				else
+					$('.card' + k + ' .verso').show()
+					$('.cards').css('pointer-events','auto')
+			# else l = 0
+			console.log(score)
--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,25 @@
 (function() {
   $(function() {
-    var cards, fisherYates, getgame;
-    cards = [['carta1', 'carta2', 'carta3', 'carta4', 'carta5', 'carta6', 'carta7', 'carta8'], ['cartaA', 'cartaB', 'cartaC', 'cartaD', 'cartaE', 'cartaF', 'cartaG', 'cartaH']];
+    var cards, fisherYates, i, l, score, _i, _results;
+    cards = [['<img class="verso" src="assets/img/verso.png"><img src="assets/img/c1.png">', '<img class="verso" src="assets/img/verso.png"><img src="assets/img/c2.png">', '<img class="verso" src="assets/img/verso.png"><img src="assets/img/c3.png">', '<img class="verso" src="assets/img/verso.png"><img src="assets/img/c4.png">', '<img class="verso" src="assets/img/verso.png"><img src="assets/img/c5.png">', '<img class="verso" src="assets/img/verso.png"><img src="assets/img/c6.png">', '<img class="verso" src="assets/img/verso.png"><img src="assets/img/c7.png">', '<img class="verso" src="assets/img/verso.png"><img src="assets/img/c8.png">'], ['<img class="verso" src="assets/img/verso.png"><img src="assets/img/c1.png">', '<img class="verso" src="assets/img/verso.png"><img src="assets/img/c2.png">', '<img class="verso" src="assets/img/verso.png"><img src="assets/img/c3.png">', '<img class="verso" src="assets/img/verso.png"><img src="assets/img/c4.png">', '<img class="verso" src="assets/img/verso.png"><img src="assets/img/c5.png">', '<img class="verso" src="assets/img/verso.png"><img src="assets/img/c6.png">', '<img class="verso" src="assets/img/verso.png"><img src="assets/img/c7.png">', '<img class="verso" src="assets/img/verso.png"><img src="assets/img/c8.png">']];
+    score = 0;
+
+    /* Inicia o jogo */
     $('.start').on('click', function() {
+      var allCards, i, shuffleCards, _i, _ref, _results;
       $(this).parent().fadeOut();
       $('.game').fadeIn();
-      return getgame();
-    });
-    getgame = function() {
-      var allCards, i, shuffleCards, _i, _ref;
       allCards = cards[0].concat(cards[1]);
       shuffleCards = fisherYates(allCards);
+      _results = [];
       for (i = _i = 0, _ref = shuffleCards.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
-        $('.cards .card' + i).html(shuffleCards[i]);
+        _results.push($('.cards .card' + i).html(shuffleCards[i]));
       }
-      return console.log(shuffleCards);
-    };
-    return fisherYates = function(arr) {
+      return _results;
+    });
+
+    /* Algoritmo de ordenação Fisher Yates */
+    fisherYates = function(arr) {
       var i, j, _ref;
       i = arr.length;
       if (i === 0) {
@@ -28,6 +31,29 @@
       }
       return arr;
     };
+    _results = [];
+    for (i = _i = 0; _i < 16; i = ++_i) {
+      l = 0;
+      _results.push($('.card' + i).on('click', function() {
+        var k;
+        l++;
+        k = $(this).attr('class').split('card')[1];
+        $('.card' + k + ' .verso').hide();
+        $('.card' + k).addClass('escolha' + l);
+        if (l > 0 && l < 3) {
+          console.log(l);
+          $('.cards').css('pointer-events', 'none');
+          if ($('.escolha1') === $('.escolha2')) {
+            score++;
+          } else {
+            $('.card' + k + ' .verso').show();
+            $('.cards').css('pointer-events', 'auto');
+          }
+        }
+        return console.log(score);
+      }));
+    }
+    return _results;
   });
 
 }).call(this);
